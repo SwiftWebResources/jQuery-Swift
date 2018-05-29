8059757f7d5ecebe99199ed4a1d42e0850f3718d
Helge Heß <devteam@zeezide.de>
2018-05-29 14:50:54 +0200

    Add jQuery 3.1.1

---
+++ Sources/jQuery/ResourceMap.swift
@@ -0,0 +1,26 @@
+// Generated on Tue May 29 14:50:33 CEST 2018
+//
+
+import struct Foundation.Data
+
+public extension jQuery {
+
+  /**
+   * Lookup a jQuery resource. The returned Data
+   * contains the gzip compressed resource.
+   *
+   * Available resource names:
+   *
+   * - "jquery.min.js"
+   *
+   */
+  public static func resourceNamed(_ name: String) -> Data? {
+    switch name {
+
+      case "jquery.min.js":
+        return data_jquery_min_js
+
+      default: return nil
+    }
+  }
+}
