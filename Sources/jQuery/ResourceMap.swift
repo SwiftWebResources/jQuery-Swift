383d80ba7797a3e48de2135f464def70a84a4f6b
Helge Heß <me@helgehess.eu>
2020-12-26 13:07:38 +0100

    Update to jQuery 3.5.1
    Only the latest is the greatest.

---
+++ Sources/jQuery/ResourceMap.swift
@@ -0,0 +1,26 @@
+// Generated on Sat Dec 26 13:07:15 CET 2020
+//
+
+import struct Foundation.Data
+
+public extension jQuery {
+
+  /**
+   * Lookup a jQuery resource. The returned Data
+   * contains the gzip compressed resource.
+   *
+   * Available resource names:
+   *
+   * - "jquery.min.js"
+   *
+   */
+  static func resourceNamed(_ name: String) -> Data? {
+    switch name {
+
+      case "jquery.min.js":
+        return data_jquery_min_js
+
+      default: return nil
+    }
+  }
+}
